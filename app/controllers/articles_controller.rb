7d2f4cc066c470cae4d7fb17c91f9430997fcc71
shusakukamiyama <xyz000shupi000@gmail.com>
2019-06-21 23:48:38 +0900

    add Ransack

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,8 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
+    @search = Article.search(params[:q])
+    @articles = @search.result
 
     if params[:tag_name]
       @articles = @articles.tagged_with("#{params[:tag_name]}")
