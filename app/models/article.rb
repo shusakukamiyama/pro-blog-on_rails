139f7b6cb6bac94cbea828b2326de0195d608bfe
shusakukamiyama <xyz000shupi000@gmail.com>
2019-06-25 11:12:33 +0900

    add category

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
     acts_as_taggable
     is_impressionable counter_cache: true
     mount_uploader :picture, PictureUploader
+    belongs_to :category
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_23_132858) do
+ActiveRecord::Schema.define(version: 2019_06_24_093604) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2019_06_23_132858) do
     t.datetime "updated_at", null: false
     t.string "picture"
     t.integer "impressions_count"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "impressions", force: :cascade do |t|
